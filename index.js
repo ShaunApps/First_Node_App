b1880dbc0db85bb2aa90eb8b73624c3a66171a50
ShaunApps <shaun.apps@gmail.com>
2016-02-21 16:17:10 -0500

    fixed issue with redis

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var redis = require('redis');
+var redis = require("redis");
 var redisClient = redis.createClient();
 
 
-// var messages = []; was used to store messages locally
+var messages = [];
 var storeMessage = function(name, data) {
-  var message = JSON.stringify(({name: name, data: data});
+  var message = JSON.stringify({name: name, data: data});
 
   redisClient.lpush("messages", message, function(err, response){
     redisClient.ltrim("messages", 0, 9);
@@ -34,17 +34,31 @@ io.on('connection', function(socket) {
   socket.on('join', function(name) {
     socket.nickname = name;
     socket.broadcast.emit('chat message', name + " has joined the chat");
-    messages.forEach(function(message) {
-      socket.emit('chat message', message.name + ": " + message.data);
+
+    socket.broadcast.emit('add chatter', name);
+    redisClient.smembers('names', function(err, names){
+      names.forEach(function(name){
+        socket.emit('add chatter', name);
+      });
+
     });
+    redisClient.sadd('chatters', name);
+
+
+   redisClient.lrange("messages", 0, -1, function(err, messages){
+    messages = messages.reverse();
+
+     messages.forEach(function(message) {
+       message = JSON.parse(message);
+       socket.emit('chat message', message.name + ": " + message.data);
+     });
+   });
   });
 
   // socket.on('disconnect', function (){
   //   socket.nickname = name;
   //   socket.emit('chat message', name + " has left the chat");
   // });
-
-
 
 });
 
