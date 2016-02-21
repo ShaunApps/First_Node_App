902847895e07f6df1a91bb23289c9fc6709b184c
ShaunApps <shaun.apps@gmail.com>
2016-02-21 12:15:54 -0500

    stored previous 10 message in server to display on connection, installed Redis

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var redis = require('redis');
+var client = redis.createClient();
+
+
+var messages = [];
+var storeMessage = function(name, data) {
+  messages.push({name: name, data: data});
+  if (messages.length > 10) {
+    messages.shift();
+  }
+}
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -8,18 +19,29 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket) {
 
+
   socket.on('chat message', function(msg) {
 
     var nickname = socket.nickname;
 
     socket.broadcast.emit('chat message', nickname + ": " + msg);
     socket.emit('chat message', nickname + ": " + msg);
+    storeMessage(nickname, msg);
   });
+
 
   socket.on('join', function(name) {
     socket.nickname = name;
     socket.broadcast.emit('chat message', name + " has joined the chat");
+    messages.forEach(function(message) {
+      socket.emit('chat message', message.name + ": " + message.data);
+    });
   });
+
+  // socket.on('disconnect', function (){
+  //   socket.nickname = name;
+  //   socket.emit('chat message', name + " has left the chat");
+  // });
 
 
 
